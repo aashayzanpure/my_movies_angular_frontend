a0718649e4f4c53f5e7b8cee860ad88ebb695575
aashayzanpure <aashayz28@gmail.com>
2020-06-20 21:44:57 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { ApiService } from './api.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [ApiService]
+})
+export class AppComponent {
+  movies = [{title :''}];
+  selectedMovie;
+
+  constructor(private api: ApiService) {
+    this.getMovies();
+    this.selectedMovie = {id: -1, title: '', desc: '', year: 0};
+  }
+  getMovies = () => {
+    this.api.getAllMovies().subscribe(
+      data => {
+        this.movies = data;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+}
